e6e913363a79a136c61a15a0d9d1f808334dc4b7
Muhamad Ramdani <mramdani551@gmail.com>
2022-06-19 00:33:53 +0700

    Migration

--- database/migrations/2022_06_17_161600_create_product_categories_table.php
+++ database/migrations/2022_06_17_161600_create_product_categories_table.php
@@ -13,14 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->foreignId('product_id')
+                ->constrained('products');
+            $table->string('type')->nullable();
+            $table->double('price');
+            $table->integer('stock')->default(0);
+            $table->timestamps();
+            $table->index('price');
         });
     }
 
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('product_categories');
     }
 };
